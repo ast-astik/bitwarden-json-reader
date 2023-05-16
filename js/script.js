d496124ab9812fada919dc56697dcb2915fd6fd6
ASTIK <astik.ytt@gmail.com>
2023-05-16 14:50:40 +0200

    Add popup open and close functions

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const popup = document.querySelector(".popup");
+
+
+
+function openPopup() {
+	popup.classList.add("popup_open");
+}
+
+function closePopup() {
+	popup.classList.remove("popup_open");
+}
+
+
+
+// Closing the popup when clicking on the background 
+// Checking that the mousedown and mouseup events are triggered on the same element
+// If you simply use the click event, a bug appears.
+document.addEventListener("mousedown", event => {
+
+	if (event.target.classList.contains("popup")) {
+
+		let checkMouseUp = function(event) {
+
+			if (event.target.classList.contains("popup")) {
+				closePopup();
+			}
+
+			document.removeEventListener("mouseup", checkMouseUp);
+		}
+
+		document.addEventListener('mouseup', checkMouseUp);
+	}
+});
